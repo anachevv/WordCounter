f59bfbf934e7dc74386eec58aec6a141f5209906
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2023-04-21 13:13:39 +0300

    files added

---
+++ src/word_counter/app/Main.java
@@ -0,0 +1,7 @@
+package word_counter.app;
+
+public class Main {
+    public static void main(String[] args) {
+        ;;
+    }
+}
---
+++ src/word_counter/app/WordCounterFunction.java
@@ -0,0 +1,7 @@
+package word_counter.app;
+
+public class WordCounterFunction {
+    public static void main(String[] args) {
+        ;;
+    }
+}
